20ced2b03b9b6dd85a78f43b0887a252ff0f1b29
DESKTOP-ED8VI87\User <dydrkfl078@naver.com>
2023-07-10 17:52:07 +0900

    2,3,4번 질문까지 작성, 색상 변경

--- lib/yongjun_personal.dart
+++ lib/yongjun_personal.dart
@@ -25,13 +25,15 @@ class YongjunPersonal extends StatelessWidget {
                   height: 200,
                   alignment: Alignment.topCenter,
                   decoration: BoxDecoration(
-                    color: Colors.grey,
+                    color: Color(0xeeeeeeee),
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
                       "저의 MBTI는 'INTJ'입니다 사람들이랑 어울리는 걸 좋아해서 E라고 생각한 적 있는데 낯 가림이 심한 걸 보니 I가 맞는 것 같아요",
-                      style: TextStyle(color: Colors.black, fontSize: 16),
+                      style: TextStyle(
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 16),
                     ),
                   ),
                 ),
@@ -40,15 +42,68 @@ class YongjunPersonal extends StatelessWidget {
                   height: 15,
                 ),
                 Container(
-                  width: 360,
-                  height: 202,
-                  decoration: BoxDecoration(color: Colors.grey),
+                  width: 500,
+                  height: 200,
+                  alignment: Alignment.topCenter,
+                  decoration: BoxDecoration(
+                    color: Color(0xeeeeeeee),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "객관적으로 봤을 때 저의 장점은 감정적이지 않은 것입니다.",
+                      style: TextStyle(
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 500,
+                  height: 15,
                 ),
                 Container(
-                  width: 360,
-                  height: 202,
-                  decoration: BoxDecoration(color: Colors.grey),
-                )
+                  width: 500,
+                  height: 200,
+                  alignment: Alignment.topCenter,
+                  decoration: BoxDecoration(
+                    color: Color(0xeeeeeeee),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "저는 수용을 잘하는 스타일인 것 같습니다 아는 것이 적은데 내 의견을 얘기하다가 부끄러워지는 상황이 두려워서 조심하는 것 같네요",
+                      style: TextStyle(
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 500,
+                  height: 15,
+                ),
+                Container(
+                  width: 500,
+                  height: 200,
+                  alignment: Alignment.topLeft,
+                  decoration: BoxDecoration(
+                    color: Color(0xeeeeeeee),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "개인 블로그는 운영하지 않습니다.. SNS도 안해요",
+                      style: TextStyle(
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 500,
+                  height: 15,
+                ),
               ],
             ),
           ),
