fee3be6a1679bd38aefefc8694b36725f4dccefb
LeeChoongHwan <choo901@naver.com>
2023-07-10 20:05:47 +0900

    merge with main

--- lib/choonghwan_personal.dart
+++ lib/choonghwan_personal.dart
@@ -8,8 +8,99 @@ class ChoongHwanPersonal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text("이충환 페이지"),
-    );
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Stack(children: [
+                  Container(
+                    color: Color(0xFFEEEEEE),
+                    height: 200,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "저를 소개합니다.<이충환>",
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        Text(""),
+                        Text("MBTI: ISFJ(용감한 수호자)",
+                            style: TextStyle(fontSize: 20)),
+                        Text(""),
+                      ],
+                    ),
+                  ),
+                ]),
+                Stack(children: [
+                  Container(
+                    color: Color(0xFFEEEEEE),
+                    height: 200,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "저를 소개합니다.<이충환>",
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        Text(""),
+                        Text("MBTI: ISFJ(용감한 수호자)",
+                            style: TextStyle(fontSize: 20)),
+                        Text(""),
+                      ],
+                    ),
+                  ),
+                ]),
+                Stack(children: [
+                  Container(
+                    color: Color(0xFFEEEEEE),
+                    height: 200,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "저를 소개합니다.<이충환>",
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        Text(""),
+                        Text("MBTI: ISFJ(용감한 수호자)",
+                            style: TextStyle(fontSize: 20)),
+                        Text(""),
+                      ],
+                    ),
+                  ),
+                ]),
+                Stack(children: [
+                  Container(
+                    color: Color(0xFFEEEEEE),
+                    height: 200,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "저를 소개합니다.<이충환>",
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        Text(""),
+                        Text("MBTI: ISFJ(용감한 수호자)",
+                            style: TextStyle(fontSize: 20)),
+                        Text(""),
+                      ],
+                    ),
+                  ),
+                ]),
+              ],
+            ),
+          ),
+        ));
   }
 }
