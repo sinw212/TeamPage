2d0599ed8eb441693aebfea91c0396c1b1b10be3
DESKTOP-ED8VI87\User <dydrkfl078@naver.com>
2023-07-10 17:07:54 +0900

    이용준 정보 추가

--- lib/yongjun_personal.dart
+++ lib/yongjun_personal.dart
@@ -7,8 +7,53 @@ class YongjunPersonal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text("이용준 페이지"),
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+        title: Text("이용준의 프로필"),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 500,
+                  height: 200,
+                  alignment: Alignment.topCenter,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "저의 MBTI는 'INTJ'입니다 사람들이랑 어울리는 걸 좋아해서 E라고 생각한 적 있는데 낯 가림이 심한 걸 보니 I가 맞는 것 같아요",
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 500,
+                  height: 15,
+                ),
+                Container(
+                  width: 360,
+                  height: 202,
+                  decoration: BoxDecoration(color: Colors.grey),
+                ),
+                Container(
+                  width: 360,
+                  height: 202,
+                  decoration: BoxDecoration(color: Colors.grey),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
